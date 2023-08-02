03fdcb9880cccec5ab2a68e83a070ba6936cca67
WYNNEPUI <wynnepui@gmail.com>
2023-08-02 13:06:45 +0800

    yes

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,3 +1,22 @@
+from pathlib import Path
+import csv
+
+# create a file to csv file.
+fp = Path.cwd()/"Cash_On_Hand.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and sales record
+    cashonhand=[] 
+
+    # append time sheet and sales record into the salesRecords list
+    for row in reader:
+        #get the day and cash on hand for each day
+        cashonhand.append([row[0],row[1]])  
+print(cashonhand) 
 
 
 
@@ -12,8 +31,5 @@
 
 
 
-
-
-print("hi mum")
 
 
