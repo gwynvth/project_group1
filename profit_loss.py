31d5947a38143be863b37671f75190339ad30bfd
gwynvth <gwynethseetoh@gmail.com>
2023-08-02 17:13:00 +0800

    me

--- profit_loss.py
+++ profit_loss.py
@@ -1,1 +1,27 @@
-print("hello world")
+import csv
+from pathlib import Path
+
+# create a file to csv file. 
+fp = Path.cwd()/"csv_reports"/"Profits_And_Loss.csv"
+
+# read the csv file to append profit and quantity from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and sales record
+    ProfitsandLoss=[] 
+
+    # append time sheet and sales record into the salesRecords list
+    for row in reader:
+        #get the employee id, toal hours, break hours, and sales for each record
+        #and append the salesRecords list
+        ProfitsandLoss.append([row[0],row[1],row[2],row[3], row[4]])   
+
+# print(ProfitsandLoss)
+
+
+# for days in ProfitsandLoss:
+
+
+
