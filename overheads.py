8993d15de29db43e703fc56e3d80bbdbfc3fbb51
JinglinTa <tanjinglebell05@gmail.com>
2023-08-03 16:10:34 +0800

    hhhh

--- overheads.py
+++ overheads.py
@@ -1,1 +1,25 @@
-print("shalla")
+import csv
+from pathlib import Path
+
+# create a file to csv file. 
+fp = Path.cwd()/"csv_reports"/"Overheads.csv"
+
+# read the csv file to append profit and quantity  from the csv.
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader) # skip header
+
+    # create an empty lists to store time sheet and sales record
+    Overheads=[] 
+
+    # append time sheet and sales record into the salesRecords list
+    for row in reader:
+        #get the employee id, toal hours, break hours, and sales for each record
+        #and append the salesRecords list
+        Overheads.append([row[0],row[1]])  
+
+print(Overheads)
+
+
+# for days in Overheads:
+
