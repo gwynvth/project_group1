91966c6ad5b339aee98cff202914670cb4727539
WYNNEPUI <wynnepui@gmail.com>
2023-08-07 15:40:03 +0800

    hello

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -19,7 +19,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         cashonhand.append([row[0],row[1]])   
 print(cashonhand)
 
-# def cash_on_hand_data(cashonhand):
+# method 1) def cash_on_hand_data(cashonhand):
 #     highestincrement=0
 #     difference=[]
 
@@ -32,35 +32,72 @@ print(cashonhand)
 #             highestincrementday=item
 #     return difference, highestincrementday, highestincrement
 
-max_increment = 0
-max_increment_day = None
-max_increment_amount = None
 
-# Initialize variables for calculating the difference in Cash-on-Hand
-previous_cash_on_hand = None
-total_cash_difference = 0
-cash_on_hand = row[1]
+#method 2
+#max_increment = 0
+#max_increment_day = None
+#max_increment_amount = None
+
+ #Initialize variables for calculating the difference in Cash-on-Hand
+#previous_cash_on_hand = None
+#total_cash_difference = 0
+#cash_on_hand = row[1]
 
 # Loop through the data
-for day, cash_on_hand in cashonhand:
-    if previous_cash_on_hand is not None:
-        difference = previous_cash_on_hand - cash_on_hand
-        total_cash_difference += difference
-        if difference > max_increment:
-            max_increment = difference
-            max_increment_day = day
-            max_increment_amount = previous_cash_on_hand
+#for day in cashonhand:
+#    if previous_cash_on_hand is not None:
+#        difference = previous_cash_on_hand - cash_on_hand
+#        total_cash_difference += difference
+#        if difference > max_increment:
+#            max_increment = difference
+#            max_increment_day = day
+#            max_increment_amount = previous_cash_on_hand
 
-    previous_cash_on_hand = cash_on_hand
+#    previous_cash_on_hand = cash_on_hand
 
 # Calculate the final Cash-on-Hand
-final_cash_on_hand = int(previous_cash_on_hand + total_cash_difference)
+#final_cash_on_hand = int(previous_cash_on_hand + total_cash_difference)
 
 # Output the results
-print(f"Difference in Cash-on-Hand: {int(total_cash_difference)}")
-print(f"Final Cash-on-Hand: {final_cash_on_hand}")
-print(f"Day of the highest increment: {max_increment_day}")
-print(f"Amount of the highest increment: {int(max_increment_amount)}")
+#print(f"Difference in Cash-on-Hand: {int(total_cash_difference)}")
+#print(f"Final Cash-on-Hand: {final_cash_on_hand}")
+#print(f"Day of the highest increment: {max_increment_day}")
+#print(f"Amount of the highest increment: {int(max_increment_amount)}")
+
+
+
+#method 3
+def compute_difference(cashonhand):
+    highest_increment = 0
+    highest_increment_day = None
+    differences = []
+
+    for item in range(1, len(cashonhand)):
+        day, coh = int(cashonhand[item][0]), int(cashonhand[item][1])
+        prev_coh = int(cashonhand[item - 1][1])
+
+        difference = coh - prev_coh
+        differences.append((day, difference))
+
+        if difference > highest_increment:
+            highest_increment = difference
+            highest_increment_day = day
+
+    return differences, highest_increment_day, highest_increment
+
+differences, highest_increment_day, highest_increment = compute_difference(cashonhand)
+
+print("Differences in COH between consecutive days:")
+for day, difference in differences:
+    print(f"Day {day}: {difference}")
+
+print(f"\nDay with the highest increment: Day {highest_increment_day}, Increment: {highest_increment}")
+
+
+
+
+
+
 
 
 
