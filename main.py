e4bb21cf14b164ac2f20d2221ad7035472188acd
gwynnethnai <gwynnethnai@gmail.com>
2023-08-08 16:19:03 +0800

    nhn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import overheads, cash_on_hand, profit_loss
 
 def main():
-    
+    overheads.Overheads_function(forex)
+    cash_on_hand.cashonhand_function(forex)
+    profit_loss.ProfitsandLoss_function(forex)
