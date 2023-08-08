60d3ae7460803dba1f4a0b4b76fc5e939c1fc3fc
gwynvth <gwynethseetoh@gmail.com>
2023-08-08 18:40:04 +0800

    me

--- overheads.py
+++ overheads.py
@@ -20,30 +20,6 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 
 #print(overheads)
 
-# def overheads(overhead_data):
-#     highest_expense_name = None
-#     highest_expense_amount = 0
-
-#     # Loop through the data to find the highest overhead expense
-#     for item in overhead_data:
-#         expense_name = item[0]
-#         expense_amount = float(item[1])
-
-#         if expense_amount > highest_expense_amount:
-#             highest_expense_amount = expense_amount
-#             highest_expense_name = expense_name
-
-#     highest_overheads = (highest_expense_amount / total_overheads) * 100 if total_overheads > 0 else 0
-
-#     return f"[HIGHEST OVERHEAD] {highest_expense_name.upper()}: {highest_overheads:.2f}%"
-
-# # Calculate the total overheads
-# total_overheads = sum(float(item[1]) for item in ooverheads)
-
-# # Call the function and print the result
-# highest_overhead_info = overheads(ooverheads)
-# print(highest_overhead_info)
-
 def overheads_function():
     highest_expense_name = None
     highest_expense_amount = 0
